26a24e7a2d3f69bb46849478696ebcca3498cc22
zminsuck <125437750+zminsuck@users.noreply.github.com>
2025-06-10 17:45:07 +0900

    main

--- src/main/java/com/survivalcoding/Cleric.java
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -1,4 +1,5 @@
 package com.survivalcoding;
+import java.util.Random;
 
 public class Cleric {
         // 클레릭 이름
@@ -12,6 +13,7 @@ public class Cleric {
         int hp = MAX_HP;
         int mp = MAX_MP;
 
+        Random random = new Random();
         // 셀프 에이드 마법으로 MP - 5소모 -> MAX_HP 까지 회복
         public void selfAid() {
                 // MAX_MP = 10 마법 실현 -> 5소모 = MAX_MP - mp 5 = 마나 5
@@ -21,5 +23,15 @@ public class Cleric {
                 // System.out.println(this.name + "셀프 에이드를 사용했다!" + this.mp + "마나 5를 소모");
                 this.hp = MAX_HP;
                 // System.out.println(this.name + "HP가 전부 회복되었다!");
+        }
+
+        public int pray(int sec) {
+                // 기도한 초만큼 랜덤하게 MP를 회복
+                this.mp += sec;
+                // 초 만큼 랜덤 수 증가
+                int recover = random.nextInt(3) * sec;
+                int Recovers = Math.min(recover, MAX_MP - mp);
+                mp += Recovers;
+                return Recovers;
         }
 }
--- src/test/java/com/survivalcoding/ClericTest.java
+++ src/test/java/com/survivalcoding/ClericTest.java
@@ -8,10 +8,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class ClericTest {
 
     @Test
-    @DisplayName("mp를 소모하여 체력을 MAX_HP 만큼 회복")
+    @DisplayName("기도를 하면 기도 (초)만큼 mp를 회복")
     void selfAid() {
         final Cleric cleric = new Cleric ();
-        cleric.selfAid();
 
         assertEquals(5, cleric.mp);
         assertEquals(50, cleric.hp);
